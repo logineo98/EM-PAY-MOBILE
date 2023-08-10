364899ce7829084c1651256780ec31abddec3ccc
Logineo <c.diabate@logineo.com>
2023-08-10 17:32:12 +0000

    travaux de la journée du 10-8-2023

--- src/components/cards/CustomDrawerContent.tsx
+++ src/components/cards/CustomDrawerContent.tsx
@@ -1,14 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const CustomDrawerContent = () => {
+
+
+
     return (
-        <View>
+        <ScrollView style={styles.container}>
             <Text>CustomDrawerContent</Text>
-        </View>
+        </ScrollView>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: { flex: 1, padding: 10 },
+})
 
 export default CustomDrawerContent
